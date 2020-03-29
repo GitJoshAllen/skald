d8dd9d44c92426784f9dc0e758646895f883a536
Joshua Clark Allen <jo2hall3n@gmail.com>
2020-03-28 19:49:05 -0500

    updated month to be accurate

--- components/today.js
+++ components/today.js
@@ -1,6 +1,6 @@
 const  GetDate = () => {
     var today = new Date();
-    return today.getMonth() + "/" + today.getDate() + "/" + today.getFullYear();
+    return (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
 }
 
 const GetHours = () => {
